7cc6a633fc8016faf75166608050d1db535cc5a2
Caleb-Niehues <73597234+Caleb-Niehues@users.noreply.github.com>
2022-11-30 18:21:33 -0600

    Test demo working

---
+++ Project/MyMainCode/625Script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#import sys - needed for python command line args
+#https://stackoverflow.com/questions/14155669/call-python-script-from-bash-with-argument
+
+echo "Hello World from bash!"
+python3 625Test.py "Hello World from the command line!"
---
+++ Project/MyMainCode/625Test.py
@@ -0,0 +1,4 @@
+import sys
+
+print("Hello World from python!")
+print(sys.argv[1])
