8b3f7788aacac3442ff37b74f717f0cf60dbace3
Caleb-Niehues <73597234+Caleb-Niehues@users.noreply.github.com>
2022-12-08 16:56:30 -0600

    Parameters made global

--- Project/MyMainCode/MasterScript.sh
+++ Project/MyMainCode/MasterScript.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 #cd /mnt/c/Users/Chenr/source/repos/625FinalProject/project/MyMainCode
 
-#We make ANGLE an environemnt variable
-#This means it can be accessed and ALTERED anywhere - be careful when altering
-ANGLE=15
-export ANGLE
+#The following lines replace the Parameters.py class - the use of the export command exposes the variables defined to all child processes
+#This means they are accessible, but if you set them in any of those files, it will be changed in all following processes
+export phaseName = "8-Paper1"
+#export phaseName = "8-Paper2"
+export vehicleType = ("SUT" "TST")
+export vehicleModel = ("01F800Truck.k" "0TractorSemi-Trailer.k")
+export barrierBoundary = ("Fixed" "Free" "Segmented")
+export barrierShape = ("TL3" "TL4" "TL5") #this is only used in phase 2
+export angleOfAttack = 15
+
 ./ExecutionScript.sh
